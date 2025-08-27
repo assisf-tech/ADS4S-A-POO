9cfcbf0ab6e3500ea156609b5ecc258e71f80330
assisf-tech <felipeassis2015.fa@gmail.com>
2025-08-27 20:29:56 -0300

    ppp

---
+++ src/main/java/com/bn/api/PessoaModel.java
@@ -0,0 +1,12 @@
+package com.bn.api;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity(name = "TBL_PESSOA")
+@Table (name = "PESSOA")
+public class PessoaModel {
+
+
+
+}
